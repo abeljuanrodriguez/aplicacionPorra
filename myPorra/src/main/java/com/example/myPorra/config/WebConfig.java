372fb5af90ee0447e08e3c9574f14a89daa6fd34
abel <abelelentrego@gmail.com>
2025-03-25 17:47:06 +0100

    CORS Spring boot

--- myPorra/src/main/java/com/example/myPorra/config/WebConfig.java
+++ myPorra/src/main/java/com/example/myPorra/config/WebConfig.java
@@ -1,22 +1,23 @@
 package com.example.myPorra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:4200",         // Entorno local de Angular
-                    "https://app-my-porra-angular.vercel.app"  // Tu dominio en Vercel
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);  // Cache de opciones CORS por 1 hora
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200","https://app-my-porra-angular.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
